0a99e541099a2938f4cec9ebcfb1b9158d94bab1
Anass Jabrane <jabraneanass12@gmail.com>
2026-02-18 12:24:20 +0000

    calcul score importance + energy avec coefficient

--- src/modules/sorter.js
+++ src/modules/sorter.js
@@ -1,8 +1,9 @@
-function getbesttask(){
+function getbesttask(tasks,enrgy){
     let besttask=null;
     let hightscore = -1000;
 
     for(let i=0 ; i<length.tasks ; i++){
         let task = tasks[i]
+        let score = (task.importance *3) + (task.enrgy *2)
     }
 }
