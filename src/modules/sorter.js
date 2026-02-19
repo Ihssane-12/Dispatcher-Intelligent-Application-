52ed6a745b6efcfa0b888ae91041935dacb13713
Anass Jabrane <jabraneanass12@gmail.com>
2026-02-19 10:01:33 +0000

    Vérifier si la tâche est prioritaire

--- src/modules/sorter.js
+++ src/modules/sorter.js
@@ -1,6 +1,6 @@
 function getbesttask(tasks,enrgy){
-    let besttask=null;
-    let hightscore = -1000;
+    let bestTask=null;
+    let highestScore = -1000;
 
     for(let i=0 ; i<length.tasks ; i++){
         let task = tasks[i]
@@ -12,6 +12,11 @@ function getbesttask(tasks,enrgy){
         }
         else if (enrgy>= 8){
             score = score - (task.effort *2);
+        }
+          // 4. Kan-choufu wach had l-tâche hiya l-wa3ra hta daba?
+        if (score > highestScore) {
+            highestScore = score;
+            bestTask = task;
         }
     }
 }
