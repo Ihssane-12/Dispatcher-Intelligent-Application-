f7dcbe71ee30e27962f76a6f1358f186e8dedc58
Ihsane Ben-Mouina <benminaihssane@email.com>
2026-02-19 10:58:15 +0000

    Add length validation

--- src/modules/validator.js
+++ src/modules/validator.js
@@ -3,6 +3,13 @@ export const Validator = {
     titleRegex: /^[a-zA-Z0-9\s]{5,50}$/,
     validateTitle: (title) => {
     const value = title.trim();
-    
+    // 1. Check Length (bin 5 w 50)
+if (value.length < 5 || value.length > 50) {
+    return {
+        isValid: false,
+        message: "Tittle khasso ikoun bin 5 w 50 characters"
+    };
+}
+
 }
 };
