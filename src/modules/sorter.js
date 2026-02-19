60466e77b26fe28d281fc3f5b839aa23827bea83
Anass Jabrane <jabraneanass12@gmail.com>
2026-02-19 15:07:00 +0000

    fixer la logique du function

--- src/modules/sorter.js
+++ src/modules/sorter.js
@@ -1,19 +1,27 @@
-function getbesttask(tasks,enrgy){
-    let bestTask=null;
+
+let energy=3;
+let tasks=[{id:122 , titre:"Anass" , importance:3, enrgy:1  }]
+
+
+
+export function getbesttask(tasks, enrgy) {
+    if (!tasks || tasks.length === 0) return null;
+
+    let bestTask = null;
     let highestScore = -1000;
 
-    for(let i=0 ; i<length.tasks ; i++){
+    for(let i=0 ; i<tasks.length ; i++){
         let task = tasks[i]
-        let score = (task.importance *3) + (task.enrgy *2);
+        let score = (task.importance *3) + ((task.urgence * 2));
 
         if(enrgy<= 3){
-            // Cas user 3yan 
+            // Cas user fatigue
             score=score - (task.effort * 5);
         }
         else if (enrgy>= 8){
             score = score - (task.effort *2);
         }
-          // 4. Kan-choufu wach had l-tâche hiya l-wa3ra hta daba?
+          // 4. defeculat taches?
         if (score > highestScore) {
             highestScore = score;
             bestTask = task;
@@ -21,3 +29,5 @@ function getbesttask(tasks,enrgy){
     }
     return bestTask;
 }
+
+console.log(getbesttask(tasks,energy));
