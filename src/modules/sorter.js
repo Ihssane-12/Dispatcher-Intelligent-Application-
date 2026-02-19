fc72fa1a961126ad881caf283420963913315e13
Anass Jabrane <jabraneanass12@gmail.com>
2026-02-19 09:47:40 +0000

    energy user

--- src/modules/sorter.js
+++ src/modules/sorter.js
@@ -4,6 +4,14 @@ function getbesttask(tasks,enrgy){
 
     for(let i=0 ; i<length.tasks ; i++){
         let task = tasks[i]
-        let score = (task.importance *3) + (task.enrgy *2)
+        let score = (task.importance *3) + (task.enrgy *2);
+
+        if(enrgy<= 3){
+            // Cas user 3yan 
+            score=score - (task.effort * 5);
+        }
+        else if (enrgy>= 8){
+            score = score - (task.effort *2);
+        }
     }
 }
