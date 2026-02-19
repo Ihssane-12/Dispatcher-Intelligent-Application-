bd1240c86e882976e98a50302f73e23acc658e2a
Ihsane Ben-Mouina <benminaihssane@email.com>
2026-02-19 10:56:51 +0000

    Add validateTitle method skeleton

--- src/modules/validator.js
+++ src/modules/validator.js
@@ -1,4 +1,8 @@
 // 🔍 Module Validator: Validation dyal l-inputs
 export const Validator = {
     titleRegex: /^[a-zA-Z0-9\s]{5,50}$/,
+    validateTitle: (title) => {
+    const value = title.trim();
+    
+}
 };
