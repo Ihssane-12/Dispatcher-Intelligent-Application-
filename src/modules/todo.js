4accbe6248ae11d47cc95a6bd92037649d03a849
Ihsane Ben-Mouina <benminaihssane@email.com>
2026-02-19 10:52:42 +0000

    Logique de suppression complète

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -2,6 +2,7 @@ import { Storage } from './storage.js';
 import { Validator } from './validator.js';
 
 export const TodoManager = {
+    
     addTask: () => {
         const titleInput = document.getElementById('task-title');
         const urgencyInput = document.getElementById('urgency-select');
@@ -11,10 +12,12 @@ export const TodoManager = {
         const titleValue = titleInput.value;
 
         const validation = Validator.validateTitle(titleValue);
+        
         if (!validation.isValid) {
-            alert(validation.message);
-            return;
+            alert(validation.message); 
+            return; 
         }
+
         const newTask = {
             id: Date.now(),
             title: titleValue.trim(),
@@ -24,36 +27,48 @@ export const TodoManager = {
             status: 'pending',
             createdAt: new Date().toISOString()
         };
+
         Storage.saveTask(newTask);
+
         titleInput.value = "";
         TodoManager.renderTasks();
         console.log("Task added successfully:", newTask);
     },
-    renderTasks: () => {
-    const tasksList = document.getElementById('all-tasks-container');
-    if (!tasksList) return;
 
-    const tasks = Storage.getTasks();
-    tasksList.innerHTML = "";
+    renderTasks: () => {
+        const tasksList = document.getElementById('all-tasks-container');
+        if (!tasksList) return; // Guard clause ila makanch l-element f l-page
+
+        const tasks = Storage.getTasks();
+
+        tasksList.innerHTML = "";
+
         tasks.forEach(task => {
-        const taskDiv = document.createElement('div');
-        taskDiv.className = 'task-card';
-        
-        taskDiv.innerHTML = `
-            <div class="task-info">
-                <p class="task-tittle-text">${task.title}</p>
-            </div>
-            <button class="delete-btn" data-id="${task.id}" title="Supprimer">
-                🗑️
-            </button>
-        `;
-        tasksList.appendChild(taskDiv);
-    });
-     TodoManager.initDeleteEvents();
+            const taskDiv = document.createElement('div');
+            taskDiv.className = 'task-card'; 
+            
+            taskDiv.innerHTML = `
+                <div class="task-info">
+                    <p class="task-tittle-text">${task.title}</p>
+                </div>
+                <button class="delete-btn" data-id="${task.id}" title="Supprimer">
+                    🗑️
+                </button>
+            `;
+            tasksList.appendChild(taskDiv);
+        });
+
+        TodoManager.initDeleteEvents();
     },
+
     initDeleteEvents: () => {
-    const deleteButtons = document.querySelectorAll('.delete-btn');
-    deleteButtons.forEach(btn => {
-        
-    })}
-}
+        const deleteButtons = document.querySelectorAll('.delete-btn');
+        deleteButtons.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                const id = parseInt(e.target.closest('button').dataset.id);
+                Storage.deleteTask(id);
+                TodoManager.renderTasks(); 
+            }, { once: true });
+        });
+    }
+};
