615cd8b6d13ebe5d2a35ca328399520e8d2a733d
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-02-19 12:18:41 +0100

    Sélection du bouton et conteneur de suggestions

--- src/modules/quiz.js
+++ src/modules/quiz.js
@@ -6,3 +6,6 @@ const result2 = document.getElementById("result2");
 
 const slider3 = document.getElementById("slider3");
 const result3 = document.getElementById("result3");
+
+const seesuggestion = document.getElementById("seesuggestion");
+const suggestions = document.getElementById("suggestion");
