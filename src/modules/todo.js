95bf766990c384b10e46dda358b526729386746f
Ihsane Ben-Mouina <benminaihssane@email.com>
2026-02-19 10:38:45 +0000

    Retrieve form inputs

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -2,5 +2,15 @@ import { Storage } from './storage.js';
 import { Validator } from './validator.js';
 
 export const TodoManager = {
-    
+     addTask: () => {
+        const titleInput = document.getElementById('task-title');
+        const urgencyInput = document.getElementById('urgency-select');
+        const importanceInput = document.getElementById('importance-select');
+        const effortInput = document.getElementById('effort-select');
+
+        const titleValue = titleInput.value;
+
+        const validation = Validator.validateTitle(titleValue);
+
+}
 }
