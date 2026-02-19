b3634ecb36b79c413f4ecebfa5fd2d91dc159f67
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-02-19 12:00:38 +0100

    Sélection des sliders et résultats

--- Quiz.js
+++ Quiz.js
@@ -1,43 +1,98 @@
+// // document.addEventListener("DOMContentLoaded", function() {
+
+// const slider = document.getElementById("slider");
+// const result = document.getElementById("result");
+
+// const slider2 = document.getElementById("slider2");
+// const result2 = document.getElementById("result2");
+
+// const slider3 = document.getElementById("slider3");
+// const result3 = document.getElementById("result3");
+
+// const seesuggestions = document.getElementById("seesuggestions");
+// const suggestions = document.getElementById("suggestions");
+
+
+// // sliders
+// result.textContent = slider.value;
+// slider.addEventListener("input", () => {
+//     result.textContent = slider.value;
+// });
+
+// result2.textContent = slider2.value;
+// slider2.addEventListener("input", () => {
+//     result2.textContent = slider2.value;
+// });
+
+// result3.textContent = slider3.value;
+// slider3.addEventListener("input", () => {
+//     result3.textContent = slider3.value;
+// });
+
+
+// // moyenne + suggestion
+// seesuggestions.addEventListener("click", () => {
+
+//     let somme =
+//         parseInt(result.value) +
+//         parseInt(result2.value) +
+//         parseInt(result3.value);
+
+//     let moyen = somme / 3;
+// });
+//  let container = document.getElementById('calcule');
+//     container.innerHTML = `Moyenne: ${moyen.toFixed(2)}`;
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+
 const slider = document.getElementById("slider");
 const result = document.getElementById("result");
-
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.getElementById("slider");
-    const result = document.getElementById("result");
-slider.addEventListener("input", () => {
-    result.textContent = slider.value;
-});
-    result.textContent = slider.value;
-});
-
-
-
 
 const slider2 = document.getElementById("slider2");
 const result2 = document.getElementById("result2");
 
-slider2.addEventListener("input", () => {
-    result2.textContent = slider2.value;
-});
-result2.textContent = slider2.value;
-
-
-
 const slider3 = document.getElementById("slider3");
 const result3 = document.getElementById("result3");
 
+const seesuggestion = document.getElementById("seesuggestion");
+const suggestions = document.getElementById("suggestion");
+
+
+
+slider.addEventListener("input", () => {
+    result.textContent = slider.value;
+    checkSomm();
+});
+
+slider2.addEventListener("input", () => {
+    result2.textContent = slider2.value;
+    checkSomm();
+});
+
 slider3.addEventListener("input", () => {
     result3.textContent = slider3.value;
+    checkSomm();
 });
-result3.textContent = slider3.value;
 
 
-const seesuggestions = document.getElementById("seesuggestions");
-const suggestions = document.getElementById("suggestions");
-seesuggestions.addEventListener("click",() => {
-    const valeur =parseInt(slider.value)
-    if (valeur <=4) {
-        suggestions.textContent="bass énergie ";
-    }else  {
-        suggestions.textContent="haute énergie";}
+// moyenne + suggestion
+let moyenneStockee = 0;
+
+function checkSomm() {
+
+    let val1 = parseInt(slider.value);
+    let val2 = parseInt(slider2.value);
+    let val3 = parseInt(slider3.value);
+
+    let somme = val1 + val2 + val3;
+     moyenneStockee = somme / 3;
+
+}
+seesuggestion.addEventListener("click", function() {
+    checkSomm();
+    console.log("moyenne stockee",moyenneStockee);
 });
+
+
+
---
+++ src/modules/quiz.js
@@ -0,0 +1,8 @@
+const slider = document.getElementById("slider");
+const result = document.getElementById("result");
+
+const slider2 = document.getElementById("slider2");
+const result2 = document.getElementById("result2");
+
+const slider3 = document.getElementById("slider3");
+const result3 = document.getElementById("result3");
