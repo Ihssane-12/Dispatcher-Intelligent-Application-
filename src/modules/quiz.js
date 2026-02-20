4f1280857c8bbf2a8607da9f45bb40b7c8638a17
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-02-20 11:12:38 +0100

    faire des modification sur le code

--- src/modules/quiz.js
+++ src/modules/quiz.js
@@ -1,45 +1,14 @@
-const slider = document.getElementById("slider");
-const result = document.getElementById("result");
+// src/modules/quiz.js
 
-const slider2 = document.getElementById("slider2");
-const result2 = document.getElementById("result2");
+export const Quiz = {
+  calculateEnergy: () => {
+    const sliders = document.querySelectorAll(".quiz-slider");
+    let total = 0;
 
-const slider3 = document.getElementById("slider3");
-const result3 = document.getElementById("result3");
+    sliders.forEach((slider) => {
+      total += parseInt(slider.value);
+    });
 
-const seesuggestion = document.getElementById("seesuggestion");
-const suggestions = document.getElementById("suggestion");
-
-slider.addEventListener("input", () => {
-    result.textContent = slider.value;
-    checkSomm();
-});
-
-slider2.addEventListener("input", () => {
-    result2.textContent = slider2.value;
-    checkSomm();
-});
-
-slider3.addEventListener("input", () => {
-    result3.textContent = slider3.value;
-    checkSomm();
-});
-
-
-let moyenneStockee = 0;
-
-function checkSomm() {
-    let val1 = parseInt(slider.value);
-    let val2 = parseInt(slider2.value);
-    let val3 = parseInt(slider3.value);
-
-    let somme = val1 + val2 + val3;
-    moyenneStockee = somme / 3;
-}
-
-
-
-seesuggestion.addEventListener("click", function() {
-    checkSomm();
-    console.log("moyenne stockee", moyenneStockee);
-});
+    return Math.round(total / sliders.length);
+  }
+};
